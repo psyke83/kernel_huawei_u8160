e5e1e2413819c7edf298da84e05ff6224acc2c36
Conn O'Griofa <connogriofa@gmail.com>
2011-10-04 00:48:59 +0100

    Use correct msm7x25 iomap

--- arch/arm/mach-msm/include/mach/msm_iomap.h
+++ arch/arm/mach-msm/include/mach/msm_iomap.h
@@ -49,6 +49,7 @@
 #include "msm_iomap-8x50.h"
 #elif defined(CONFIG_ARCH_MSM8X60)
 #include "msm_iomap-8x60.h"
+#elif defined(CONFIG_ARCH_MSM7X25)
 #include "msm_iomap-7x25.h"
 #else
 #include "msm_iomap-7xxx.h"
